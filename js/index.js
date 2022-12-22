9f22059df016519e71dc032d788ad66332922b50
Bruno Carnavalli <carnavallibruno@gmail.com>
2022-12-22 12:17:53 -0500

    added JS page swap example code

---
+++ js/index.js
@@ -0,0 +1,6 @@
+var buttonToPage2 = document.getElementById('toPage2')
+
+buttonToPage2.addEventListener("click", () => {
+  window.location.href = "http://127.0.0.1:5500/js/pagina2.html";
+})
+
---
+++ js/index2.js
@@ -0,0 +1,5 @@
+var buttonToPage1 = document.getElementById('toPage1')
+
+buttonToPage1.addEventListener("click", () => {
+  window.location.href = "http://127.0.0.1:5500/js/pagina1.html";
+})
